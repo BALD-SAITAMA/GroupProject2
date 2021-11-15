d98e363ba758458962e7b21847c246e05353e55b
Honorine Ndom <honorinendzah@gmail.com>
2021-11-14 20:00:12 -0800

    commit

--- models/Product.js
+++ models/Product.js
@@ -1,45 +1,72 @@
-const sequelize = require('../config/connection.js')
-// importing parts of the sequelize library 
-const {Model, DataTypes } = require('sequelize')
-// initializing the product model/ table and extending sequelize model class
-class Product extends Model {}
-// set up rules and fields for the product model
-Product.init(
-{
-id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-},
-product_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-},
-price: {
-    type: DataTypes.DECIMAL(10,2),
-    allowNull: false,
-    validate: {
-        isDecimal: true,
-    },
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
 
-},
-category_id: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    references: {
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
         model: 'category',
         key: 'id',
-    }
-}
-}, 
-{
-sequelize,
-timestamps: false,
-freezeTableName: true,
-underscores: true,
-modelName: 'product'
-}
-)
+      }
+    },
+    size: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    filename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    parentFolder: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
 
 module.exports = Product;
